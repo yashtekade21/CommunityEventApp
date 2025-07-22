69083e4eaf0d3eca2b223086fce807d8002c7e0e
Yash Tekade <128163324+yashtekade21@users.noreply.github.com>
2025-07-22 10:59:06 +0530

    Delete src directory

--- src/main/java/com/example/communityevents/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.communityevents.repository;
-
-import com.example.communityevents.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
--- src/main/java/com/example/communityevents/repository/LocationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.communityevents.repository;
-
-import com.example.communityevents.model.Location;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LocationRepository extends JpaRepository<Location, Long> {
-}
